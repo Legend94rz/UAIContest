a7ea9b69f56caa4e476d94feea2b853ccc2129aa
Legend <Legend94rz@gmail.com>
2017-11-06 22:19:16 +0800

    32.74

--- UAIContest/UAIContest.py
+++ UAIContest/UAIContest.py
@@ -1,1 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
 
+#plot A->B orders count by time
+trainPath1 = '.\\Data\\train_July.csv'
+trainPath2 = '.\\Data\\train_Aug.csv'
+testPath = '.\\Data\\test_id_Aug_agg_public5k.csv'
+
+def ReadTrain():
+    train1 = pd.read_csv(trainPath1)
+    train2 = pd.read_csv(trainPath2)
+    return pd.concat([train1,train2])
+
+def ReadTest():
+    return pd.read_csv(testPath)
+
+def analysis(trainSet,testSet):
+    result = pd.DataFrame()
+    result['test_id'] = testSet['test_id']
+    p=[]
+    for i in range(len(testSet)):
+        if i%500==0:
+            print('%r: %d'%(dt.datetime.now(), x))
+        x = testSet.iloc[i]
+        startId = x['start_geo_id']
+        endId = x['end_geo_id']
+        date = x['create_date']
+        hour = x['create_hour']
+        #dt.datetime.strptime(str,'%Y-%m-%d')
+        #x.replace(hour=9)
+        tmp = trainSet[(trainSet['start_geo_id']==startId) & (trainSet['end_geo_id']==endId)]\
+              .sort_values(['create_date','create_hour'])
+        s = 0
+        if(hour>0):
+            s = s + tmp[tmp['create_hour']==hour-1].shape[0]
+        if(hour<23):
+            s = s + tmp[tmp['create_hour']==hour+1].shape[0]
+        if(hour>0 and hour<23):
+            s = s/2.0;
+        p.append(s)
+    result['count']=p
+    result.to_csv('prediction.csv',encoding='utf-8',index = False)
+
+if __name__=="__main__":
+    trainSet = ReadTrain()
+    testSet = ReadTest()
+    analysis(trainSet,testSet)
