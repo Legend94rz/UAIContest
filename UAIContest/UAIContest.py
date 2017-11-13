b50f886481a4b6bc9fbe7b5019731a0221048333
legend <Legend94rz@gmail.com>
2017-11-13 20:32:13 +0800

    2.07

--- UAIContest/UAIContest.py
+++ UAIContest/UAIContest.py
@@ -10,12 +10,14 @@ testPath = '.\\Data\\test_id_Aug_agg_public5k.csv'
 def ReadTrain():
     train1 = pd.read_csv(trainPath1)
     train2 = pd.read_csv(trainPath2)
-    return pd.concat([train1,train2])
+    return train1,train2
 
 def ReadTest():
     return pd.read_csv(testPath)
 
-def analysis(trainSet,testSet):
+
+def analysis(trainJuly,trainAug,testSet):
+    trainSet = pd.concat([trainJuly,trainAug])
     result = pd.DataFrame()
     result['test_id'] = testSet['test_id']
     p=[]
@@ -25,22 +27,36 @@ def analysis(trainSet,testSet):
         x = testSet.iloc[i]
         startId = x['start_geo_id']
         endId = x['end_geo_id']
-        tmp = trainSet[(trainSet['start_geo_id']==startId) & (trainSet['end_geo_id']==endId) & (trainSet['create_hour']==int(x['create_hour']))]\
+        tmp = trainSet[(trainSet['start_geo_id']==startId) & (trainSet['end_geo_id']==endId) ]\
+              .groupby(['create_date','create_hour']).size().reset_index(name='count')
+        X = range(24*38)
+        Y = [0 for j in range(24*38)]
+        for j in range(tmp.shape[0]):
+            date = dt.datetime.strptime(tmp['create_date'][j],'%Y-%m-%d')
+            date = date.replace(hour=int(tmp['create_hour'][j]))
+            delta = date-dt.datetime(2017,7,1,0)
+            Y[ delta.days*24 + int(delta.seconds/3600)  ] = tmp['count'][j]
+        
+        plt.plot(X,Y)
+        plt.show()
+
+def GenResult(trainJuly,trainAug,testSet):
+    trainSet = pd.concat([trainJuly,trainAug])
+    result = pd.DataFrame()
+    result['test_id'] = testSet['test_id']
+    p=[]
+    for i in range(len(testSet)):
+        if i%500==0:
+            print('%s: %d'%(dt.datetime.now(), i))
+        x = testSet.iloc[i]
+        startId = x['start_geo_id']
+        endId = x['end_geo_id']
+        tmp = trainSet[(trainSet['start_geo_id']==startId) & (trainSet['end_geo_id']==endId) & (trainSet['create_hour']==int(x['create_hour'])) ]\
               .groupby('create_date').size().reset_index(name='count')
-        #X = range(24*38)
-        #Y = [0 for j in range(24*38)]
-        #for j in range(tmp.shape[0]):
-        #    date = dt.datetime.strptime(tmp['create_date'][j],'%Y-%m-%d')
-        #    date = date.replace(hour=int(tmp['create_hour'][j]))
-        #    delta = date-dt.datetime(2017,7,1,0)
-        #    Y[ delta.days*24 + int(delta.seconds/3600)  ] = tmp['count'][j]
-        #
-        #plt.plot(X,Y)
-        #plt.show()
-        if len(tmp['count']<100)>0:
-            q = tmp[tmp['count']<100].mean().item()
+        if len(tmp['count']<50)>0:
+            q = tmp[tmp['count']<50].mean().item()
             if q<3:
-                q = tmp[tmp['count']<100]['count'].mode().mean()
+                q = tmp[tmp['count']<50]['count'].mode().mean()
         else:
             q=0
         p.append( q )
@@ -48,8 +64,8 @@ def analysis(trainSet,testSet):
     result['count']=p
     result.to_csv('prediction.csv',index=False)
 
-
 if __name__=="__main__":
-    trainSet = ReadTrain()
+    trainJuly,trainAug = ReadTrain()
     testSet = ReadTest()
-    analysis(trainSet,testSet)
+    #analysis(trainJuly,trainAug,testSet)
+    GenResult(trainJuly,trainAug,testSet)
