a1640fbb5e9503c5c8dbeaba471971f2bf4d100b
legend <Legend94rz@gmail.com>
2017-11-28 10:26:56 +0800

    重构Generator

--- UAIContest/DatasetGenerator.py
+++ UAIContest/DatasetGenerator.py
@@ -1,10 +1,8 @@
-import pandas as pd
 import datetime as dt
-import numpy as np
 import pickle
 from multiprocessing.pool import Pool
 from multiprocessing import cpu_count
-import matplotlib.pyplot as plt
+import pandas as pd
 
 train_july = '.\\Data\\train_July.csv'
 train_aug = '.\\Data\\train_Aug.csv'
@@ -15,16 +13,56 @@ augset = pd.read_csv(train_aug)
 testset = pd.read_csv(testFile)
 trainset = pd.concat([julyset, augset])
 
-X = []
-Y = []
-TX = []
+finished = 0
+def log_result(result):
+    global finished
+    finished = finished + 1
+    if finished % 10 == 0:
+        print("%s finished %d\n"%(dt.datetime.now(),finished))
+def GenTrainingSet(filename, interface):
+    try:
+        dic = pickle.load(open(filename+'-train.pkl','rb'))
+        return dic['X'],dic['Y']
+    except IOError:
+        pass
+    print("Gening Training set...\n")
+    global finished
+    finished = 0
+    result = []
+    pool = Pool(cpu_count()-1)
+    for i in range(len(testset)):
+        result.append( interface(pool, tuple(testset.loc[i,['start_geo_id','end_geo_id','create_date','create_hour']]) ) )
+    pool.close()
+    pool.join()
+    #result = interface()
+    X = [result[i].get()[0] for i in range(len(result))]
+    Y = [result[i].get()[1] for i in range(len(result))]
+    pickle.dump({'X':X,'Y':Y},open(filename+'-train.pkl','wb'))
+    return X,Y
+def GenTestSet(filename, interface):
+    try:
+        tdic = pickle.load(open(filename+'-test.pkl','rb'))
+        return tdic['X']
+    except IOError:
+        pass
+    print("Gening Test set...\n")
+    global finished
+    finished = 0
+    result = []
+    pool = Pool(cpu_count()-1)
+    for i in range(len(testset)):
+        result.append( interface( pool , tuple(testset.loc[i,['start_geo_id','end_geo_id','create_date','create_hour']]) ) )
+    pool.close()
+    pool.join()
+    #result = interface()
+    pickle.dump({'X':TX},open(filename+'-test.pkl','wb'))
+    TX = [result[i].get()[0] for i in range(len(result))]
+    return TX
 
-def WorkerForTrain(*x):
-    """
-    x - [start_geo_id, end_geo_id,create_date, create_hour]
-    """
+####    Gen for split 012       ###########
+def Split012ForTrain(*x):
     feature = []
-    y=[]
+    Y=[]
     time = dt.datetime.strptime(x[2],'%Y-%m-%d')+dt.timedelta(hours=int(x[3]))
     timeL = time+dt.timedelta(hours=-1)
     timeR = time+dt.timedelta(hours=1)
@@ -39,55 +77,41 @@ def WorkerForTrain(*x):
         else:
             feature.append(0)
     return (feature,Y)
-def dummy():
-    WorkerForTrain('c538ad66d710f99ad0ce951152da36a4','90bb1d035e403538d20b073aec57bea2','2017-08-01',21)
-    pass
-def CbkForTrain(result):
-    X.append(result[0])
-    Y.append(result[1])
-    if len(Y) % 100 == 0:
-        print("%s, gened train %d\n" % (dt.datetime.now(),len(Y)))
-
-def GenTrainingSet():
-    try:
-        dic = pickle.load(open('train.pkl','rb'))
-        return dic['X'],dic['Y']
-    except IOError:
-        pass
-    print("Gening Training set...\n")
-    pool = Pool(cpu_count()-1)
-    for i in range(len(testset)):
-        pool.apply_async(WorkerForTrain, tuple(testset.loc[i,['start_geo_id','end_geo_id','create_date','create_hour']]), callback=CbkForTrain)
-
-    pool.close()
-    pool.join()
-    pickle.dump({'X':X,'Y':Y},open('train.pkl','wb'))
-    return X,Y
-
-def WorkerForTest(*x):
-    """
-    x - ['start_geo_id','end_geo_id','create_date','create_hour']
-    """
+def Split012ForTest(*x):
+    #x = p[1].loc[p[2],['start_geo_id','end_geo_id','create_date','create_hour']]
     feature = [(dt.datetime.strptime(x[2],'%Y-%m-%d') - dt.datetime(2017,7,1)).days*24 + x[3]]
     return feature
+def Split012():
+    def aplyTrain(pool,params):
+        return pool.apply_async(Split012ForTrain,params,callback = log_result)
+    def aplyTest(pool,params):
+        return pool.apply_async(Split012ForTest,params,callback = log_result)
+    X,Y = GenTrainingSet('split', aplyTrain)
+    TX = GenTestSet('split',aplyTest)
+    return X,Y,TX
+###########################################
 
-def CbkForTest(result):
-    TX.append(result)
-    if len(TX) % 100 == 0:
-        print("%s, gened test %d\n" % (dt.datetime.now(),len(TX)))
+### feature is 3 vector of (31*24+7*12)
+def SyntheSet(*x):
+    feature = []
+    Y = []
+    tmpset = trainset[(trainset['start_geo_id']==x[0])&(trainset['end_geo_id']==x[1])]
+    for k in range(3):
+        t = [0 for i in range(31*24+7*12)]
+        tmp = tmpset[tmpset['status']==k].groupby(['create_date','create_hour']).size().reset_index(name='count')
+        for i in range(len(tmp)):
+            days = (dt.datetime.strptime( tmp.loc[i,'create_date'],'%Y-%m-%d') - dt.datetime(2017,7,1)).days
+            if days<31:
+                t[days*24+tmp.loc[i,'create_hour']] = tmp.loc[i,'count']
+            else:
+                t[24*31+(days-31)*12+int( tmp.loc[i,'create_hour']//2 )]=tmp.loc[i,'count']
+        feature.append(t)
+    return (feature,Y)
 
-def GenTestSet():
-    try:
-        tdic = pickle.load(open('test.pkl','rb'))
-        return tdic['X']
-    except IOError:
-        pass
-    print("Gening Test set...\n")
-    pool = Pool(cpu_count()-1)
-    for i in range(len(testset)):
-        pool.apply_async(WorkerForTest,tuple(testset.loc[i,['start_geo_id','end_geo_id','create_date','create_hour']]),callback=CbkForTest)
-    pool.close()
-    pool.join()
-    pickle.dump({'X':TX},open('test.pkl','wb'))
-    return TX
+def Synthe():
+    def aply(pool,params):
+        return pool.apply_async(SyntheSet,params,callback = log_result)
+    X,Y = GenTrainingSet('synthe',aply)
+    return X,Y,X
+
 
--- UAIContest/UAIContest.py
+++ UAIContest/UAIContest.py
@@ -1,46 +1,60 @@
 import pandas as pd
-from DatasetGenerator import GenTestSet,GenTrainingSet,dummy
-from ILearner import UseMean,Xgb,Linear,GausProc
-from sklearn.gaussian_process.kernels import ExpSineSquared,WhiteKernel
-from multiprocessing.pool import Pool
+from DatasetGenerator import Split012,Synthe
+import math
+import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
-YP = []
-def trainAndPredict(XI,YI,TXI,modeli):
-    modeli.train(XI,YI)
-    return modeli.predict(TXI)
-
-def log_result(yp):
-    YP.append(float(yp))
-    if len(YP)%100==0:
-        print(len(YP))
+#todo: remove this dependency
+testFile = '.\\Data\\test_id_Aug_agg_public5k.csv'
+testset = pd.read_csv(testFile)
 
 def GenResult(X,Y,TX):
-    for i in range(len(X)):
-        y=[]
-        for j in range(3):
-            x = X[i][j*3:3+j*3]
-            if (math.fabs(x[0]-x[1])<8):
-                y.append((x[0]+x[1])/2)
-            else:
-                y.append(x[2])
-        YP.append(sum(y))
-
-    #Gen Result:
     result = pd.DataFrame()
-    result['test_id'] = range(5000)
-    result['count']=YP
-    result.to_csv('prediction.csv',index=False,encoding='utf-8')
+    result['test_id']=range(5000)
+    yp = []
+    for i in range(len(testset)):
+        x=testset.loc[i]
+        days = (dt.datetime.strptime(x['create_date'],'%Y-%m-%d')-dt.datetime(2017,7,1)).days
+        hur = x['create_hour']
+        fe = np.array(X[i]).sum(0)
+        s = []
+        for j in range(38):
+            if j<31:
+                s.append(fe[hur+24*j])
+            else:
+                s.append( fe[31*24 + (j-31)*12 + int(hur//2)] )
+        s = np.array(s)
+        m = np.ceil( np.mean(s[s<=50]) )
+        yp.append(m)
+    result['count']=yp
+    print(np.mean(yp))
+    result.to_csv('opt_hist_ceil.csv',index=False)
 
-    #compare = pd.DataFrame()
-    #compare['pred'] = YP
-    #compare['real'] = Y
-    #compare.to_csv('compare.csv',index=False)
+def analisis(X):
+    R = pd.read_csv("1.97.csv")
+    ind = (R['count']<1.5)&(R['count']>=0.5)
+    for i in range(len(ind)):
+        if ind[i]:
+            print(testset.loc[i])
+            x = list(range(31*24))
+            for j in range(1,8):
+                if j%2==0:
+                    x.extend( range( (j+30)*24 + 1, (j+31)*24 ,2) )
+                else:
+                    x.extend( range( (j+30)*24,(j+31)*24,2))
+            s = np.sum( X[i],0)
+            y = s
+            plt.plot(x,y)
+            days = (dt.datetime.strptime(testset.loc[i,'create_date'],'%Y-%m-%d')-dt.datetime(2017,7,1)).days
+            plt.plot(days*24+testset.loc[i,'create_hour'], R.loc[i,'count'],'rx')
+            x=range(testset.loc[i,'create_hour'],31*24,24)
+            y=[s[j] for j in x]
+            plt.plot(x,y,'bx')
+            plt.show()
+
 
 if __name__=="__main__":
-    #dummy()
-    X,Y = GenTrainingSet()
-    #TX = GenTestSet()
-    GenResult(X,Y,X)
+    X,Y,TX = Synthe()
+    #GenResult(X,Y,X)
+    analisis(X)
