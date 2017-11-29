0c54a3e5891dad79fdafba695362b0128e2e2dab
legend <Legend94rz@gmail.com>
2017-11-29 16:38:20 +0800

    matlab辅助调参

--- UAIContest/UAIContest.py
+++ UAIContest/UAIContest.py
@@ -9,52 +9,116 @@ import matplotlib.pyplot as plt
 testFile = '.\\Data\\test_id_Aug_agg_public5k.csv'
 testset = pd.read_csv(testFile)
 
+def calcSimilitude(y):
+    '''
+    Y - 4 by 7 of matrix
+    '''
+    sim = 0
+    cnt = 0
+    for j in range(1,4):
+        a = np.sum(y[j] * y[j - 1])
+        b = np.linalg.norm(y[j]) * np.linalg.norm(y[j - 1])
+        if b == 0:
+            continue
+        sim = sim + (a / b)
+        cnt = cnt + 1
+    if cnt > 0:
+        return sim / cnt
+    else:
+        return 0
+
+def makeOnePrediction(X,INDEX):
+    start = testset.loc[INDEX,'start_geo_id']
+    end = testset.loc[INDEX,'end_geo_id']
+    date = testset.loc[INDEX,'create_date']
+    hur = testset.loc[INDEX,'create_hour']
+    XI = np.array(X[INDEX])
+    s = XI.sum(0)
+
+    y = np.reshape([s[j] for j in range(hur,28 * 24,24)] , (4,7))
+    sim = calcSimilitude(y)
+    meanOfWeek = y[:,3].mean()
+
+
+    preTm = dt.datetime.strptime(date,'%Y-%m-%d') + dt.timedelta(hours=int(hur - 1))
+    nxtTm = dt.datetime.strptime(date,'%Y-%m-%d') + dt.timedelta(hours=int(hur + 1))
+    pre = s[24 * 31 + 12 * (preTm - dt.datetime(2017,8,1)).days + preTm.hour // 2]
+    if  date=='2017-08-07' and hur == 23 :
+        nxt = pre
+    else:
+        nxt = s[24 * 31 + 12 * (nxtTm - dt.datetime(2017,8,1)).days + nxtTm.hour // 2]
+    meanOfPN = (nxt+pre)/2
+
+    allHis = []
+    for j in range(38):
+        if j < 31:
+            allHis.append(s[hur + 24 * j])
+        else:
+            if j%2!=hur%2:
+                allHis.append(s[31 * 24 + (j - 31) * 12 + int(hur // 2)])
+    allHis = np.array(allHis)
+    meanOfHis = 0
+    if len(allHis[(allHis>0)]) > 0:
+       meanOfHis = allHis[(allHis>0)].mean()
+
+    return sim,meanOfWeek,pre,nxt,meanOfHis
+
 def GenResult(X,Y,TX):
     result = pd.DataFrame()
-    result['test_id']=range(5000)
+    result['test_id'] = range(5000)
     yp = []
+    col = ['sim','meanOfWeek','pre','nxt','meanOfHis']
     for i in range(len(testset)):
-        x=testset.loc[i]
-        days = (dt.datetime.strptime(x['create_date'],'%Y-%m-%d')-dt.datetime(2017,7,1)).days
-        hur = x['create_hour']
-        fe = np.array(X[i]).sum(0)
-        s = []
-        for j in range(38):
-            if j<31:
-                s.append(fe[hur+24*j])
-            else:
-                s.append( fe[31*24 + (j-31)*12 + int(hur//2)] )
-        s = np.array(s)
-        m = np.ceil( np.mean(s[s<=50]) )
-        yp.append(m)
-    result['count']=yp
-    print(np.mean(yp))
-    result.to_csv('opt_hist_ceil.csv',index=False)
+        yp.append(makeOnePrediction(X,i))
+
+    yp = np.array(yp)
+    for i in range(len(col)):
+        result[col[i]] = yp[:,i]
+
+    result.to_csv('Fe.csv',index=False)
 
 def analisis(X):
     R = pd.read_csv("1.97.csv")
+    q = []
+    for i in range(len(R)):
+        print(testset.loc[i])
+        x = range(testset.loc[i,'create_hour'],28 * 24,24)
+        s = np.sum(X[i] ,0)
+        y = [s[j] for j in x]
+        q.append(calcSimilitude(y))
+    r = pd.DataFrame()
+    r['q'] = q
+    r.to_csv('q.csv')
+
+    '''
     ind = (R['count']<1.5)&(R['count']>=0.5)
-    for i in range(len(ind)):
-        if ind[i]:
+    for i in range(len(R)):
+   #for i in [0,664,665,666,680,687,736,1769]:
+        #if ind[i]:
+        if True:
             print(testset.loc[i])
-            x = list(range(31*24))
-            for j in range(1,8):
-                if j%2==0:
-                    x.extend( range( (j+30)*24 + 1, (j+31)*24 ,2) )
-                else:
-                    x.extend( range( (j+30)*24,(j+31)*24,2))
-            s = np.sum( X[i],0)
-            y = s
-            plt.plot(x,y)
-            days = (dt.datetime.strptime(testset.loc[i,'create_date'],'%Y-%m-%d')-dt.datetime(2017,7,1)).days
-            plt.plot(days*24+testset.loc[i,'create_hour'], R.loc[i,'count'],'rx')
-            x=range(testset.loc[i,'create_hour'],31*24,24)
-            y=[s[j] for j in x]
-            plt.plot(x,y,'bx')
+            f,axs = plt.subplots(3,sharey = False)
+            for k in range(3):
+                x = list(range(31*24))
+                for j in range(1,8):
+                    if j%2==0:
+                        x.extend( range( (j+30)*24 + 1, (j+31)*24 ,2) )
+                    else:
+                        x.extend( range( (j+30)*24,(j+31)*24,2))
+                s = X[i][k]
+                y = s
+                axs[k].plot(x,y)
+                days = (dt.datetime.strptime(testset.loc[i,'create_date'],'%Y-%m-%d')-dt.datetime(2017,7,1)).days
+                axs[k].plot(days*24+testset.loc[i,'create_hour'], R.loc[i,'count'],'rx')
+                x=range(testset.loc[i,'create_hour'],31*24,24)
+                y=[s[j] for j in x]
+                axs[k].plot(x,y,'bx')
+            f.subplots_adjust(hspace=0)
             plt.show()
+    '''
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     X,Y,TX = Synthe()
-    #GenResult(X,Y,X)
-    analisis(X)
+    GenResult(X,Y,X)
+    #analisis(X)
---
+++ UAIContest/UI.m
@@ -0,0 +1,163 @@
+function varargout = UI(varargin)
+% UI MATLAB code for UI.fig
+%      UI, by itself, creates a new UI or raises the existing
+%      singleton*.
+%
+%      H = UI returns the handle to a new UI or the handle to
+%      the existing singleton*.
+%
+%      UI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UI.M with the given input arguments.
+%
+%      UI('Property','Value',...) creates a new UI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before UI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to UI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help UI
+
+% Last Modified by GUIDE v2.5 28-Nov-2017 21:04:42
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @UI_OpeningFcn, ...
+                   'gui_OutputFcn',  @UI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before UI is made visible.
+function UI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to UI (see VARARGIN)
+
+% Choose default command line output for UI
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes UI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+global Fe
+load('Fe.mat','Fe');
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = UI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+	calc();
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+global objS1
+objS1 = hObject;
+
+
+% --- Executes on slider movement.
+function slider2_Callback(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+	calc();
+
+% --- Executes during object creation, after setting all properties.
+function slider2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+global objS2
+objS2 = hObject;
+
+% --- Executes on slider movement.
+function slider3_Callback(hObject, eventdata, handles)
+% hObject    handle to slider3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+	calc();
+	
+	
+% --- Executes during object creation, after setting all properties.
+function slider3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+global objS3
+objS3 = hObject;
+
+function calc()
+	global objS1
+	global objS2
+	global objS3
+	global Fe
+	res = zeros(size(Fe,1),1);
+	s = [ get(objS1,'Value'),get(objS2,'Value'),get(objS3,'Value')]
+	ind = Fe.sim>s(1);
+	vind =~(ind);
+	res(ind) = Fe.meanOfWeek(ind);
+	res(vind) = Fe.meanOfHis(vind)*s(3) + (1-s(3))*( Fe.pre(vind)*s(2) + Fe.nxt(vind)*(1-s(2)) ) ;
+	%res = round(res);
+	mean(res)
+	histogram(res(res<5),'BinWidth',0.01);
+	%save('res.mat','res');
